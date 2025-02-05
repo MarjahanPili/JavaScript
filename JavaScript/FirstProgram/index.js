a3b6430e4301bf0a3fef936282a8d387a93762b4
MarjahanPili <marjahan.pili@gmail.com>
2025-02-06 00:15:15 +0600

    variable,object,array

---
+++ JavaScript/FirstProgram/index.js
@@ -0,0 +1,22 @@
+let name = "Marjahan";
+let age = "20";
+let date = "05-02-2025";
+
+console.log(name);
+console.log(age);
+console.log(date);
+
+//Print all valriable at a time
+console.log(name,age,date);
+
+// print array
+let arr = [10,20,30]
+console.log(arr);
+console.log(arr[2]);
+
+//print object
+let obj = {a:100};
+//obj.a = 200;
+//console.log(obj.a);
+//console.log(obj.a);
+console.log(obj);
